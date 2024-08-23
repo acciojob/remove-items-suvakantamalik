a4821cacbc502e6f8deec34983ce7b0abfd2b959
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-23 17:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let select = document.getElementById("colorSelect");
+select.addEventListener("click", function(event) {
+	select.remove(select.selectedIndex);
+});
